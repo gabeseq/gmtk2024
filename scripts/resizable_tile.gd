b68f589f39e1fbf69bf893229a6ea4dab740225e
Gabe <74687304+gabeseq@users.noreply.github.com>
2024-08-18 21:38:05 -0400

    full level of tiles but 1 is broken for some reason

--- scripts/resizable_tile.gd
+++ scripts/resizable_tile.gd
@@ -3,10 +3,12 @@ extends Polygon2D
 @onready var hitbox: CollisionPolygon2D = $Area2D/CollisionPolygon2D
 @onready var tile: Polygon2D = $"."
 @onready var snap_points: Node2D = $"../SnapPoints"
+@onready var INITIAL_SCALE = tile.global_scale
 
 const DEFAULT_SCALE = Vector2(1,1)
 const MIN_SCALE = 0.5
-const MAX_SCALE = 1.75
+const MAX_SCALE = 2.0
+const SCALE_INCREMENT = 0.1
 
 var dragging = false
 var hovering = false
@@ -19,41 +21,43 @@ var update_scale = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	tile.scale = DEFAULT_SCALE
 	hitbox.polygon = tile.polygon
+	hitbox.global_position = tile.global_position
+	hitbox.scale = tile.scale
+	tile.global_scale = DEFAULT_SCALE
 	for pt in snap_points.get_children():
 		if pt.get_meta("id") == tile.get_meta("id"):
 			my_snap_point = pt
 			
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if my_snap_point != null and abs(tile.position - my_snap_point.position) < Vector2(32, 32):
+func _process(_delta: float) -> void:
+	hitbox.polygon = tile.polygon
+	hitbox.global_position = tile.global_position
+	hitbox.scale = tile.scale
+	if my_snap_point != null and abs(tile.position - my_snap_point.position) < Vector2(32, 32) and tile.global_scale == INITIAL_SCALE:
 		tile.color = Color(0, 255, 0)
 		is_snapped = true
 	else:
-		tile.color = Color(0,128,128)
+		tile.color = Color(1,0,1)
 		is_snapped = false
 	
 	if is_snapped and not dragging:
 		tile.position = my_snap_point.position
 	pass
 	
-
 func _input(event):
 	if hovering and Input.is_action_pressed("ScrollUp"):
 		if scale_factor == MAX_SCALE:
 			pass
 		get_bigger()
-		tile.scale = DEFAULT_SCALE * scale_factor
-		hitbox.polygon = tile.polygon
+		tile.global_scale = DEFAULT_SCALE * scale_factor
 		tile.global_position = event.global_position
 	if hovering and Input.is_action_pressed("ScrollDown"):
 		if scale_factor == MIN_SCALE:
 			pass
 		get_smaller()
-		tile.scale = DEFAULT_SCALE * scale_factor
-		hitbox.polygon = tile.polygon
+		tile.global_scale = DEFAULT_SCALE * scale_factor
 		tile.global_position = event.global_position
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if (hovering):
@@ -78,11 +82,11 @@ func _on_area_2d_mouse_exited() -> void:
 	hovering = false
 
 func get_smaller() -> void:
-	scale_factor -= 0.05
+	scale_factor -= SCALE_INCREMENT
 	if scale_factor < MIN_SCALE:
 		scale_factor = MIN_SCALE
 	
 func get_bigger() -> void:
-	scale_factor += 0.05
+	scale_factor += SCALE_INCREMENT
 	if scale_factor > MAX_SCALE:
 		scale_factor = MAX_SCALE
